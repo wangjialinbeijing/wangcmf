9c779bf4bfbd4901645313317ea5527eb18e2530
wangjialin <dz5362@qq.com>
2017-10-17 16:53:29 +0800

    很好很强大

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Admin extends Controller
+{
+    protected function _initialize()
+    {
+        parent::_initialize();
+        //TODO
+    }
+}
