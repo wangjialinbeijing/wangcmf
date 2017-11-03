77106e6a67adc3d6932628126d6aac71c5454fa1
wangjialin <dz5362@qq.com>
2017-11-03 10:24:48 +0800

    ADMIN子菜单完成

--- application/common.php
+++ application/common.php
@@ -51,30 +51,57 @@ if(!function_exists('data_auth_sign'))
 	}
 }
 
-/*
- * 数组转换为Tree
- */
-function list_to_tree($list, $pk='id', $pid = 'pid', $child = '_child', $root = 0) {
-	// 创建Tree
-	$tree = array();
-	if(is_array($list)) {
-		// 创建基于主键的数组引用
-		$refer = array();
-		foreach ($list as $key => $data) {
-			$refer[$data[$pk]] =& $list[$key];
-		}
-		foreach ($list as $key => $data) {
-			// 判断是否存在parent
-			$parentId =  $data[$pid];
-			if ($root == $parentId) {
-				$tree[] =& $list[$key];
-			}else{
-				if (isset($refer[$parentId])) {
-					$parent =& $refer[$parentId];
-					$parent[$child][] =& $list[$key];
+if(!function_exists('list_to_tree'))
+{
+	/*
+	 * 数组转换为Tree
+	 */
+	function list_to_tree($list, $pk = 'id', $pid = 'pid', $child = '_child', $root = 0)
+	{
+		// 创建Tree
+		$tree = [];
+		if (is_array($list)) {
+			// 创建基于主键的数组引用
+			$refer = [];
+			foreach ($list as $key => $data) {
+				$refer[ $data[ $pk ] ] =& $list[ $key ];
+			}
+			foreach ($list as $key => $data) {
+				// 判断是否存在parent
+				$parentId = $data[ $pid ];
+				if ($root == $parentId) {
+					$tree[] =& $list[ $key ];
+				} else {
+					if (isset($refer[ $parentId ])) {
+						$parent =& $refer[ $parentId ];
+						$parent[ $child ][] =& $list[ $key ];
+					}
 				}
 			}
 		}
+		return $tree;
 	}
-	return $tree;
+}
+
+if(!function_exists('show_menu_active'))
+{
+	/**
+	 * 菜单选中效果
+	 * @param $sub_menu
+	 * @param $active_url
+	 * @return bool
+	 */
+	function show_menu_active($sub_menu, $active_url)
+	{
+		$active = false;
+		if (!empty($sub_menu) && $active_url) {
+			foreach ($sub_menu as $key => $val) {
+				if ($active_url == $val['url']) {
+					$active = true;
+					break;
+				}
+			}
+		}
+		return $active;
+	}
 }
